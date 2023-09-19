2effa346cd196c7e7eba941f0da4820f60c5c4c8
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-09-18 20:57:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,30 @@
 import streamlit as st
-import PyPDF2
-import pandas as pd
+import tabula
 
-# Function to extract text from a PDF file
-def extract_text_from_pdf(pdf_file):
-    pdf_text = ""
-    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-    
-    for page_num in range(pdf_reader.numPages):
-        page = pdf_reader.getPage(page_num)
-        pdf_text += page.extractText()
-    
-    return pdf_text
+def pdf_to_tables(pdf_file):
+    # Use tabula to extract tables from the PDF
+    tables = tabula.read_pdf(pdf_file, pages='all', multiple_tables=True)
+    return tables
 
-# Function to convert extracted text into a DataFrame
-def convert_to_dataframe(pdf_text):
-    # Split the text into lines and create a list of rows
-    rows = pdf_text.split('\n')
-    
-    # Split each row into columns using a delimiter (e.g., tab or comma)
-    # Modify the split logic based on your PDF's structure
-    data = [row.split('\t') for row in rows]
-    
-    # Create a DataFrame from the list of data
-    df = pd.DataFrame(data, columns=["Column 1", "Column 2", "Column 3"])  # Modify column names as needed
-    
-    return df
+st.title("PDF to Tables Converter")
 
-# Streamlit App
-st.title("PDF to Table Converter")
+uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
-# Upload a PDF file
-pdf_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+if uploaded_file is not None:
+    st.subheader("Uploaded PDF:")
+    st.write(uploaded_file)
 
-if pdf_file is not None:
-    # Extract text from the PDF
-    pdf_text = extract_text_from_pdf(pdf_file)
+    # Check if the user has uploaded a file
+    if st.button("Convert to Tables"):
+        try:
+            tables = pdf_to_tables(uploaded_file)
 
-    # Convert extracted text into a DataFrame
-    df = convert_to_dataframe(pdf_text)
-
-    # Display the DataFrame
-    st.write("Converted Data:")
-    st.write(df)
-
-    # You can perform further data analysis or visualization here as needed
+            if not tables:
+                st.warning("No tables found in the PDF.")
+            else:
+                st.subheader("Tables Extracted:")
+                for i, table in enumerate(tables):
+                    st.write(f"Table {i + 1}")
+                    st.write(table)
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
