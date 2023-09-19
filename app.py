60c71ff9faf611eff0cd13812db0e04b8ad300f1
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-09-18 21:05:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,36 @@
 import streamlit as st
-import tabula
-import openjdk=8
+import pdfplumber
+import pandas as pd
 
-def pdf_to_tables(pdf_file):
-    # Use tabula to extract tables from the PDF
-    tables = tabula.read_pdf(pdf_file, pages='all', multiple_tables=True)
+# Function to extract tables from a PDF and convert them into Pandas DataFrames
+def extract_tables(pdf_file):
+    tables = []
+    with pdfplumber.open(pdf_file) as pdf:
+        for page in pdf.pages:
+            page_tables = page.extract_tables()
+            if page_tables:
+                tables.extend(page_tables)
     return tables
 
+# Streamlit UI
 st.title("PDF to Tables Converter")
 
+# Upload PDF file
 uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
 if uploaded_file is not None:
-    st.subheader("Uploaded PDF:")
-    st.write(uploaded_file)
+    st.write("You've uploaded a PDF file!")
 
-    # Check if the user has uploaded a file
-    if st.button("Convert to Tables"):
-        try:
-            tables = pdf_to_tables(uploaded_file)
+    # Extract tables from the PDF
+    tables = extract_tables(uploaded_file)
 
-            if not tables:
-                st.warning("No tables found in the PDF.")
-            else:
-                st.subheader("Tables Extracted:")
-                for i, table in enumerate(tables):
-                    st.write(f"Table {i + 1}")
-                    st.write(table)
-        except Exception as e:
-            st.error(f"An error occurred: {str(e)}")
+    if not tables:
+        st.write("No tables found in the PDF.")
+    else:
+        st.write(f"Number of tables found: {len(tables)}")
+
+        # Display tables as DataFrames
+        for i, table in enumerate(tables):
+            df = pd.DataFrame(table[1:], columns=table[0])
+            st.write(f"Table {i + 1}:")
+            st.write(df)
